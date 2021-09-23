2f5e9af8f36fb2494368681c59913ce705f4eb41
KovalchukAndrew <dry.st.jim@gmail.com>
2021-09-23 21:27:25 +0300

    change-task-status

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -49,7 +49,7 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             if (task) {
                 task.isDone = action.isDone;
             }
-            return stateCopy;
+            return {...state, [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : {...t})};
         }
         case 'CHANGE-TASK-TITLE': {
             const stateCopy = {...state};
